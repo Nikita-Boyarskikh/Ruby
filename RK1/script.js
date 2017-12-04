11d176a14f73e42c67a36276a7644b6aa9fb3cdc
Nikita-Boyarskikh <N02@yandex.ru>
2017-12-04 18:56:58 +0300

    RK1 is done

--- RK1/script.js
+++ RK1/script.js
@@ -1,9 +1,32 @@
-function script() {
-  const result = document.getElementById('result');
+function findLetters(letters, sentence) {
+    const result = [];
+    sentence.split(/[^\w]/).filter(s => s).forEach((str, i) => {
+        if (i % 2 === 1)
+    {
+        letters.split('').forEach(letter => {
+            if(~str.toLowerCase().indexOf(letter)) {
+        result.push(letter);
+    }
+    });
+    }
+});
 
-  result.innerText = "Clicked!";
+    return result.filter((v, i, a) => a.indexOf(v) === i);
+}
+
+function script() {
+    const result = document.getElementById('result');
+    const data = document.getElementById('data').value;
+    const letters = findLetters("aeijouy", data);
+    let output = '';
+    if (letters.length === 0) {
+        output = 'Not found latin voice letters';
+    } else {
+        output = letters.join();
+    }
+    result.innerText = 'Результат: ' + output;
 }
 
 window.onload = function () {
-  document.getElementById('start').onclick = script;
+    document.getElementById('start').onclick = script;
 };
